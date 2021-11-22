638ab0020fe7a7340e1a2e830346dcbf47252d05
Tauheed Khan Mohd <tauheed.del@gmail.com>
2021-11-22 16:29:25 -0600

    Added code

---
+++ AdapterDesignPattern/src/designpattern/adapter/EnemyAttacker.java
@@ -0,0 +1,14 @@
+package designpattern.adapter;
+// This is the Target Interface : This is what the client
+// expects to work with. It is the adapters job to make new 
+// classes compatible with this one.
+
+public interface EnemyAttacker {
+	
+	public void fireWeapon();
+	
+	public void driveForward();
+	
+	public void assignDriver(String driverName);
+	
+}
